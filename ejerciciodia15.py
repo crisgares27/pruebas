e37376cf23770897c11e84fbe329900ccc70c2b6
Cristina.Marco <crisgares27@gmail.com>
2021-03-17 12:52:46 +0100

    Ejercicioevaluable

--- ejerciciodia15.py
+++ ejerciciodia15.py
@@ -1,3 +1,31 @@
+#from flask import Flask, jsonify
+from flask import request
+from random import randint
+#app = Flask(__name__)
+
+
+#@app.route('/random')
+#def random_number():
+    #first_num = int(request.args.get('first_num'))
+    #second_num = int(request.args.get('second_num'))
+    #pupil_name = request.args.get('pupil_name')
+    #print(f'{pupil_name} HA HECHO LA REQUEST')
+    #return str(randint(first_num, second_num))
+
+
+#@app.route('/order', methods=['POST'])
+#def order():
+    # data = request.json
+    # numbers = data.get('numbers')
+    # numbers = list(set(numbers))
+    #numbers.sort(reverse=True)
+    #print(numbers)
+
+    # return jsonify(data)
+
+
+#app.run(host='0.0.0.0', port=80, debug=True)
+
 from flask import Flask, jsonify
 from flask import request
 from random import randint
@@ -18,10 +46,9 @@ def order():
     data = request.json
     numbers = data.get('numbers')
     numbers = list(set(numbers))
-    #numbers.sort(reverse=True)
-    print(numbers)
-
-    return jsonify(data)
+    numbers.sort(reverse=False)
+    # Establezco reverse False porque en el ejercicio se indica que sea de menor a mayor.
+    return jsonify(numbers)
 
 
 app.run(host='0.0.0.0', port=80, debug=True)
